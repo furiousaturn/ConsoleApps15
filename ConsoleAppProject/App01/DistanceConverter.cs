7cd3c3bdc27f2bc61d2fffb7d975cc8d86bfc47b
furiousaturn <71845711+furiousaturn@users.noreply.github.com>
2021-01-30 14:35:03 +0000

    Updated last commit (removed syntax errors)

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -3,18 +3,36 @@
 namespace ConsoleAppProject.App01
 {
     /// <summary>
-    /// Converts Miles to Feet (this can be updated)
+    /// This app will prompt the user to input a distance
+    /// measured in one uit (fromUnit) and it will calculate 
+    /// and output the equivalent distance in another unit (toUnit)
     /// </summary>
     /// <author>
     /// Andrew Stephenson version 0.1 (this can be updated each time)
     /// </author>
     public class DistanceConverter
     {
+        
+        //Distance conversion constants
+
         public const int FEET_IN_MILES = 5280;
         public const double METRES_IN_MILES = 1609.34;
-        private double miles;
-        private double feet;
-        private double metres;
+        public const double FEET_IN_METRES = 3.28084;
+
+        //Distance Unit Names
+
+        public const string FEET = "Feet";
+        public const string MILES = "Miles";
+        public const string METRES = "Metres";
+
+        // Distance variables
+
+        private double fromDistance;
+        private double toDistance;
+
+        //Unit variables
+        private string fromUnit;
+        private string toUnit;
 
         /// <summary>
         /// This method will input the distance measured in miles
@@ -24,11 +42,8 @@ namespace ConsoleAppProject.App01
         public void MilesToFeet()
         {
             OutputHeading("Converting Miles to Feet");
-            
             miles = InputDistance("Please enter the number of Miles > ");
-            
             CalculateFeet();
-
             OutputDistance(miles,nameof(miles),feet,nameof(feet));
         }
 
