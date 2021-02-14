c45c54d656baee69d22940236fcd5e29c9258009
furiousaturn <71845711+furiousaturn@users.noreply.github.com>
2021-02-14 08:14:23 +0000

    End of Web Part 3 - done and working, validation missing?

--- ConsoleAppProject/App01/DistanceUnits.cs
+++ ConsoleAppProject/App01/DistanceUnits.cs
@@ -1,14 +1,16 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppProject.App01
 {
     /// <summary>
     /// List of units used to measure distance
     /// </summary>
     public enum DistanceUnits
     {
+        [Display(Name ="No Unit")]
         NoUnit,
         Feet,
         Metres,
-        Kilometres,
         Miles
     }
 }
