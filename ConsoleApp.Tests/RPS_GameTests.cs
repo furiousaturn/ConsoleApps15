c1b086c96a66d2bde94b996e4d42fdc40381961c
furiousaturn <71845711+furiousaturn@users.noreply.github.com>
2021-02-16 13:24:23 +0000

    Completed the game model

---
+++ ConsoleApp.Tests/RPS_GameTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppProject.App05;
+
+
+namespace ConsoleApp.Tests
+{
+    [TestClass]
+    public class RPS_GameTests
+    {
+        private Game game = new Game("Fred");
+        
+        [TestMethod]
+        public void TestGameStart()
+        { 
+            //Act
+            game.Start();
+
+            //Assert
+            Assert.AreEqual(0, game.Human.Score);
+            Assert.AreEqual(0, game.Computer.Score);
+            Assert.AreEqual(1, game.Round);
+
+            Assert.AreEqual(GamePlayers.Player, game.CurrentPlayer.PlayerType);
+
+            Assert.AreEqual("Fred", game.Human.Name);
+            Assert.AreEqual("Computer", game.Computer.Name);
+        }
+
+        [TestMethod]
+        public void TestComputerChoiceIsRandom()
+        {
+            //Arrange
+            int[] choices = new int[4];
+            
+            //Act
+            game.Start();
+            for(int i = 0; i < 10000; i++)
+            {
+                game.MakeComputerChoice();
+                int choiceNo = (int)game.Computer.Choice;
+                choices[choiceNo]++;
+            }
+
+            //Assert
+            Assert.IsTrue(choices[0] == 0);
+            Assert.IsTrue(choices[1] > 3000);
+            Assert.IsTrue(choices[2] > 3000);
+            Assert.IsTrue(choices[3] > 3000);
+        }
+
+        [TestMethod]
+        public void TestScoreRoundRockPaper()
+        {
+            //Arrange
+            game.Start();
+            game.Human.Choice = GameChoices.Rock;
+            game.Computer.Choice = GameChoices.Paper;
+
+            //Act
+            game.ScoreRound();
+
+            //Assert
+            Assert.AreEqual(0, game.Human.Score);
+            Assert.AreEqual(2, game.Computer.Score);
+            Assert.AreEqual(game.Winner.PlayerType, GamePlayers.Computer);
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
--- ConsoleAppProject/App05/Game.cs
+++ ConsoleAppProject/App05/Game.cs
@@ -7,7 +7,7 @@ namespace ConsoleAppProject.App05
     /// <summary>
     /// 
     /// </summary>
-    class Game
+    public class Game
     {
         //Attributes
         public const int MaximumRound = 10;
@@ -21,24 +21,116 @@ namespace ConsoleAppProject.App05
 
         public Player Winner { get; set; }
 
+        private Random generator = new Random();
+
+        public int LastRound { get; set; }
+
+
         /// <summary>
         /// Constructor
         /// </summary>
         public Game(string name)
         {
-            Human = new Player("Fred", GamePlayers.Player);
+            Human = new Player(name, GamePlayers.Player);
             Computer = new Player("Computer", GamePlayers.Computer);
             Round = 0;
          }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public void Start()
         {
             Round = 1;
+            LastRound = 5;
             CurrentPlayer = Human;
             Human.Score = 0;
             Computer.Score = 0;
+        }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public void MakeComputerChoice()
+        {
+            int choice = generator.Next(1,4);
 
+            switch(choice)
+            {
+                case 1: Computer.Choice = GameChoices.Paper; break;
+                case 2: Computer.Choice = GameChoices.Rock; break;
+                case 3: Computer.Choice = GameChoices.Scissors; break;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void ScoreRound()
+        {
+            if (Human.Choice == GameChoices.Rock &&
+                Computer.Choice == GameChoices.Paper)
+            {
+                Computer.Score += 2;
+                Winner = Computer;
+            }
+            else if (Human.Choice == GameChoices.Paper &&
+                Computer.Choice == GameChoices.Rock)
+            {
+                Human.Score += 2;
+                Winner = Human;
+            }
+            else if (Human.Choice == GameChoices.Rock &&
+                Computer.Choice == GameChoices.Scissors)
+            {
+                Human.Score += 2;
+                Winner = Human;
+            }
+            else if (Human.Choice == GameChoices.Scissors &&
+                Computer.Choice == GameChoices.Rock)
+            {
+                Computer.Score += 2;
+                Winner = Computer;
+            }
+            else if (Human.Choice == GameChoices.Paper &&
+                Computer.Choice == GameChoices.Scissors)
+            {
+                Computer.Score += 2;
+                Winner = Computer;
+            }
+            else if (Human.Choice == GameChoices.Scissors &&
+                Computer.Choice == GameChoices.Paper)
+            {
+                Human.Score += 2;
+                Winner = Human;
+            }
+            
+            //capture 3 more possibilities - a tie.
+            else
+            {
+                Human.Score += 1;
+                Computer.Score += 1;
+                Winner = null;
+            }
+
+            if (Round < LastRound)
+            {
+                Round++;
+            }
+            else End();
+        }
+
+        public void End()
+        {
+            if (Computer.Score > Human.Score)
+            {
+                Winner = Computer;
+            }
+            else if (Computer.Score < Human.Score)
+            {
+                Winner = Human;
+            }
+            else Winner = null;
         }
     }
 }
