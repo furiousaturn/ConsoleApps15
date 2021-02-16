c32aead35284cb90a46a4c94e18ffabcbbbd58eb
furiousaturn <71845711+furiousaturn@users.noreply.github.com>
2021-02-16 06:18:11 +0000

    Refactored App04 into Superclass Pos nad child classes MessagePost and PhotoPost

---
+++ ConsoleAppProject/App04/Post.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppProject.App04
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Post
+    {
+        private int likes;
+        private readonly List<String> comments;
+
+        // username of the post's author
+        public String Username { get; }
+        public DateTime Timestamp { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public Post(string author)
+        {
+            this.Username = author;
+            Timestamp = DateTime.Now;
+
+            likes = 0;
+            comments = new List<String>();
+        }
+
+        /// <summary>
+        /// Record one more 'Like' indication from a user.
+        /// </summary>
+        public void Like()
+        {
+            likes++;
+        }
+
+        ///<summary>
+        /// Record that a user has withdrawn his/her 'Like' vote.
+        ///</summary>
+        public void Unlike()
+        {
+            if (likes > 0)
+            {
+                likes--;
+            }
+        }
+
+        ///<summary>
+        /// Add a comment to this post.
+        /// </summary>        
+        public void AddComment(String text)
+        {
+            comments.Add(text);
+        }
+
+        ///<summary>
+        /// Create a string describing a time point in the past in terms 
+        /// relative to current time, such as "30 seconds ago" or "7 minutes ago".
+        /// Currently, only seconds and minutes are used for the string.
+        /// </summary>     
+        private String FormatElapsedTime(DateTime time)
+        {
+            DateTime current = DateTime.Now;
+            TimeSpan timePast = current - time;
+
+            long seconds = (long)timePast.TotalSeconds;
+            long minutes = seconds / 60;
+
+            if (minutes > 0)
+            {
+                return minutes + " minutes ago";
+            }
+            else
+            {
+                return seconds + " seconds ago";
+            }
+
+        }
+
+        ///<summary>
+        /// Display the details of this post.
+        /// 
+        /// (Currently: Print to the text terminal. This is simulating display 
+        /// in a web browser for now.)
+        ///</summary>
+        public void Display()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"    Author: {Username}");
+            Console.WriteLine($"    Time Elpased: {FormatElapsedTime(Timestamp)}");
+            Console.WriteLine();
+
+            if (likes > 0)
+            {
+                Console.WriteLine($"    Likes:  {likes}  people like this.");
+            }
+            else
+            {
+                Console.WriteLine();
+            }
+
+            if (comments.Count == 0)
+            {
+                Console.WriteLine("    No comments.");
+            }
+            else
+            {
+                Console.WriteLine($"    {comments.Count}  comment(s). Click here to view.");
+            }
+        }
+    }
+}
