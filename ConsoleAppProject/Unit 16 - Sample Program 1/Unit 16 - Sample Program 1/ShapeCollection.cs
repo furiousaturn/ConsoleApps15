0918c94d3e9d9393c54d8a7d4d328b39c7763d80
Andrew Stephenson <amstephenson@burnley.ac.uk>
2022-02-08 18:32:51 +0000

    upload sample program 1

---
+++ ConsoleAppProject/Unit 16 - Sample Program 1/Unit 16 - Sample Program 1/ShapeCollection.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit_16___Sample_Program_1
+{
+    class ShapeCollection
+    {
+
+        List<Shape> shapeList = new List<Shape>();
+
+        public void AddShape(Shape shapeToAdd)
+        {
+            shapeList.Add(shapeToAdd);
+        }
+
+        public float GetShapesTotalArea()
+        {
+            float totalArea = 0;
+
+            for(int i = 0; i < shapeList.Count; i++)
+            {
+                totalArea += shapeList[i].CalcuateArea();
+            }
+            return totalArea;
+        }
+
+        public List<Shape> GetShapesList()
+        {
+            return this.shapeList;
+        }
+    }
+}
---
+++ ConsoleAppProject/Unit 16 - Sample Program 1/Unit 16 - Sample Program 1/Triangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit_16___Sample_Program_1
+{
+    class Triangle : Shape
+    {
+        public Triangle()
+        {
+            this.type = "Triangle";
+        }
+        public override float CalcuateArea()
+        {
+            float answer = this.getHeight() * this.getWidth();
+            answer /= 2;
+            return answer;
+        }
+        public override float CalculateCircumference()
+        {
+            return base.CalculateCircumference();
+        }
+    }
+}
