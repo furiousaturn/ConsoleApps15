0e03b21930438b0ce7656c04ba1f492979ff288d
Andrew Stephenson <amstephenson@burnley.ac.uk>
2022-02-08 18:17:13 +0000

    added sample program 1

---
+++ ConsoleAppProject/JAVA/Unit 16 - Sample Program 1/Unit 16 - Sample Program 1/Square.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit_16___Sample_Program_1
+{
+    class Square : Shape
+    {
+        public Square(){
+            type = "Square";
+        }
+        public override float CalcuateArea()
+        {
+            return this.getHeight() * this.getWidth();
+        }
+
+        public override float CalculateCircumference()
+        {
+            return base.CalculateCircumference();
+        }
+    }
+}
